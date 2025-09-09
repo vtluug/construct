1225e73de6451286fa15d9b739a97ada034e606c
mehbark <terezi@pyrope.net>
2025-09-09 03:59:27 -0400

    hosts/blockbuster: fix zfs

--- hosts/vesuvius/zfs.nix
+++ hosts/vesuvius/zfs.nix
@@ -5,4 +5,13 @@
   environment.systemPackages = [ pkgs.zfs ];
 
   networking.hostId = "eaab8b4b";
+
+  systemd.services.zfs-mount.enable = false;
+
+  services.zfs.autoScrub.enable = true;
+
+  fileSystems."/mount/forge" = {
+    device = "forge";
+    fsType = "zfs";
+  };
 }
