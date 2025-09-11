ddde4f0f899ee48af11fa61bd11b040ef180e2d9
mehbark <terezi@pyrope.net>
2025-09-11 15:35:55 -0400

    hosts/vesuvius: ipa further work

--- hosts/vesuvius/ipa.nix
+++ hosts/vesuvius/ipa.nix
@@ -1,6 +1,10 @@
 # TODO: /etc/krb5.keytab missing, maybe agenix
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
+  age.secrets."krb5.keytab".file = ../../secrets/krb5.keytab.age;
+
+  environment.variables.KRB5_KTNAME = config.age.secrets."krb5.keytab".path;
+
   security.ipa = {
     enable = true;
 
