abaeb5a820f0d2dfc3199a99665e4d3542e598cd
mehbark <terezi@pyrope.net>
2025-09-10 20:01:12 -0400

    hosts/bastille: add blade names

---
+++ hosts/bastille/blade-names.nix
@@ -0,0 +1,16 @@
+# keep-sorted start
+[
+  "backbiter"
+  "damocles"
+  "durendal"
+  "eyelander"
+  "excalibur"
+  "gram"
+  "gryffindor"
+  "kusanagi"
+  "narsil"
+  "oathbringer"
+  "riptide"
+  "sting"
+]
+# keep-sorted end
