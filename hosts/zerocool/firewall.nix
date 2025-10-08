01fac59e79a6072faad7813713da78ba01012239
rsk <rsk@vtluug.org>
2025-10-08 02:26:04 -0400

    broken! hosts/zerocool: add wireguard, with shellshock as a peer
    this does not work for two reasons:
    1) it will not build -- secrets/zerocool_wg.nix is
        not checked in to the repo, for obvious
        reasons. need an agenix person to come fix.
    2) shellshock does not have us as a peer
    
    those aside, it works fine. a note for the future,
     we don't need keepalive when both routers are
     globally routable.

--- hosts/zerocool/firewall.nix
+++ hosts/zerocool/firewall.nix
@@ -4,6 +4,7 @@
     enable = true;
     allowPing = true;
     trustedInterfaces = [ lan_iface ];
-    allowedTCPPorts = [ 22 2222 ];
+    allowedTCPPorts = [ 22 2222 ]; # ssh
+    allowedUDPPorts = [ 51820 ]; # wg
   };
 }
