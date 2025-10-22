253db6e1f4a1de5cbe77dfaa24f4364c6faa43cb
rsk <rsk@vtluug.org>
2025-10-22 11:33:22 -0400

    hosts/zerocool: correct agenix pub key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   vesuvius = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOHI7ziwxkEbJzvpaZulPFpDW7l0vbGJ+ifHcHJ2fHex";
-  zerocool = "";
+  zerocool = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+60yHIqES3Dr1Upp23QGwzvqELQEeH6e4lTKTV9iUY root@zerocool";
 in {
   "krb5.keytab.age".publicKeys = [ vesuvius ];
-  "zerocool/wg.priv.age".publicKeys = [ vesuvius ];
+  "zerocool/wg.priv.age".publicKeys = [ zerocool ];
 }
