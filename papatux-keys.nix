1f2c25938762eaf23159479056261758091dde62
rsk <rsk@vtluug.org>
2025-12-09 02:11:17 -0500

    keys: add jpo

--- papatux-keys.nix
+++ papatux-keys.nix
@@ -2,4 +2,5 @@
   "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIN+Xl9xdmuWyDZaNb1haR7dLnWenrwHVPgAQBM06kbWpAAAABHNzaDo= ssh: rsk@yubikey"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOen0Hh9uoJlT9ikRmc9dZQfsjMtBc2v7PNIHyf0KSNh mikhail@licas.dev"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICyTxPV3S7ms0AJ0tduI3aJP3o2TJCnkirWKaj5i1+DW clj c@pyrope.net"
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHBzpC6FBOKNzO6AfdfGna6MLRg0cf+jhr+DgUwQqU1W timelord"
 ]
