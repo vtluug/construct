59c983337d5b37530cd3338d15f9e3a770152767
mkl <mikhail@licas.dev>
2026-01-23 14:50:51 -0500

    hosts/bastille: disabled eno1 on lenovo nodes and removed auto-hostname script in favor of dhcp hostnames

--- hosts/bastille/blade.nix
+++ hosts/bastille/blade.nix
@@ -1,12 +1,15 @@
 { modulesPath, pkgs, lib, ... }: {
   imports = [
-    ./auto-hostname.nix
+    ./eno1-imm-disable.nix
     (import ../common/k3s.nix {})
     ../common/nix.nix
     ../common/sshd.nix
     ../common/users-local.nix
     (modulesPath + "/installer/netboot/netboot-minimal.nix")
   ];
+
+  # Get hostname from DHCP request
+  networking.hostName = "";
 
   # when making the ISO, the initialHashedPassword is set to "" for some reason
   # we already set a hashed password, so null this
