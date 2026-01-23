790c86e812d3d5f311de0f44e0b96600c182fc9b
rsk <rsk@vtluug.org>
2026-01-23 03:16:07 -0500

    refactor(hosts/vesuvius): use roles/common stuff

--- hosts/vesuvius/configuration.nix
+++ hosts/vesuvius/configuration.nix
@@ -7,6 +7,10 @@
     ./zfs.nix
     ./ipa.nix
     ./netboot.nix
+
+    ../common/tz-locale.nix
+    ../common/users-local.nix
+    ../common/sshd.nix
   ];
 
   boot.loader.systemd-boot.enable = true;
@@ -17,44 +21,11 @@
   networking.networkmanager.enable = true;
   networking.networkmanager.unmanaged = [ "interface-name:enp1s0f1" ];
 
-  time.timeZone = "America/New_York";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  users.users.papatux = {
-    isNormalUser = true;
-    description = "papatux";
-    extraGroups = [ "networkmanager" "wheel" ];
-    openssh.authorizedKeys.keys = import ../../papatux-keys.nix;
-  };
-
-  security.sudo.wheelNeedsPassword = false;
-
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
     neovim
   ];
 
-  services.openssh.enable = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 }
