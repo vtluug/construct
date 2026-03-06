902140361f9b6c83ca70dce2b0e2d1fb23e4051d
mkl <mikhail@licas.dev>
2026-03-06 18:22:19 -0500

    hosts/zerocool: cleaned up firewall

--- hosts/zerocool/firewall.nix
+++ hosts/zerocool/firewall.nix
@@ -14,29 +14,6 @@ let
     )
   );
 
-  generateInterVlanBlockPairings =
-    list:
-    builtins.concatLists (
-      map (
-        x:
-        map (
-          y:
-          ''iifname "vlan${x.fst}" oifname "vlan${y.fst}" drop comment "Drop vlan${x.fst} to vlan${y.fst} traffic"''
-        ) (builtins.filter (y: x.fst != y.fst) list)
-      ) list
-    );
-
-  interVlanRoutingBlock = lib.strings.concatStringsSep "\n" (
-    generateInterVlanBlockPairings taggedVlans
-  );
-
-  untaggedInterVlanBlock = lib.strings.concatStringsSep "\n" (
-    builtins.map (e: ''
-      iifname "vlan${e.fst}" oifname "${lanIface}" drop comment "Drop vlan${e.fst} to ${lanIface} traffic"
-      iifname "${lanIface}" oifname "vlan${e.fst}" drop comment "Drop ${lanIface} to vlan${e.fst} traffic"
-    '') taggedVlans
-  );
-
   routerAccess = lib.strings.concatStringsSep "\n" (
     builtins.map (
       e: ''iifname { "vlan${e.fst}" } accept comment "Allow vlan${e.fst} to access the router"''
@@ -50,51 +27,17 @@ let
   );
 
   deniedVlanDhcpv4Access = lib.strings.concatStringsSep "\n" (
-    builtins.map
-      (e: ''
-        iifname { "vlan${e.fst}" } udp dport { 53, 67 } accept comment "Allow vlan${e.fst} DHCP and DNS access the router"
-        iifname { "vlan${e.fst}" } tcp dport 53 accept comment "Allow vlan${e.fst} TCP DNS access the router"
-      '')
-      (
-        builtins.filter (
-          e:
-          !e.snd.allowRouterAccess && (builtins.hasAttr "dhcpv4" e.snd)
-        ) taggedVlans
-      )
+    builtins.map (e: ''
+      iifname { "vlan${e.fst}" } udp dport { 53, 67 } accept comment "Allow vlan${e.fst} DHCP and DNS access the router"
+      iifname { "vlan${e.fst}" } tcp dport 53 accept comment "Allow vlan${e.fst} TCP DNS access the router"
+    '') (builtins.filter (e: !e.snd.allowRouterAccess && (builtins.hasAttr "dhcpv4" e.snd)) taggedVlans)
   );
 
   deniedVlanDhcpv6Access = lib.strings.concatStringsSep "\n" (
-    builtins.map
-      (e: ''
-        iifname { "vlan${e.fst}" } udp dport { 53, 547 } accept comment "Allow vlan${e.fst} DHCP and DNS access the router"
-        iifname { "vlan${e.fst}" } tcp dport 53 accept comment "Allow vlan${e.fst} TCP DNS access the router"
-      '')
-      (
-        builtins.filter (
-          e:
-          !e.snd.allowRouterAccess && (builtins.hasAttr "dhcpv6" e.snd)
-        ) taggedVlans
-      )
-  );
-
-
-  routerForward = lib.strings.concatStringsSep "\n" (
     builtins.map (e: ''
-      iifname { "vlan${e.fst}" } oifname { "${wanIface}" } accept comment "Allow vlan${e.fst} to WAN"
-      iifname { "${wanIface}" } oifname { "vlan${e.fst}" } ct state established, related accept comment "Allow established back to vlan${e.fst}"
-    '') taggedVlans
-  );
-
-  routerIpv6IcmpAccess = lib.strings.concatStringsSep "\n" (
-    builtins.map (e: ''
-      iifname "vlan${e.fst}" ip6 nexthdr icmpv6 icmpv6 type { nd-router-solicit, nd-router-advert, nd-neighbor-solicit, nd-neighbor-advert } counter accept comment "Allow SLAAC and DHCPv6 on vlan${e.fst}"
-    '') taggedVlans
-  );
-
-  routerIpv6NdForward = lib.strings.concatStringsSep "\n" (
-    builtins.map (e: ''
-      iifname "vlan${e.fst}" ip6 nexthdr icmpv6 icmpv6 type { nd-router-solicit, nd-router-advert, nd-neighbor-solicit, nd-neighbor-advert } accept comment "Allow essential ND in FORWARD on vlan${e.fst}"
-    '') taggedVlans
+      iifname { "vlan${e.fst}" } udp dport { 53, 547 } accept comment "Allow vlan${e.fst} DHCP and DNS access the router"
+      iifname { "vlan${e.fst}" } tcp dport 53 accept comment "Allow vlan${e.fst} TCP DNS access the router"
+    '') (builtins.filter (e: !e.snd.allowRouterAccess && (builtins.hasAttr "dhcpv6" e.snd)) taggedVlans)
   );
 
   exposedUdpPorts = lib.strings.concatStringsSep "\n" (
@@ -121,7 +64,6 @@ let
     builtins.map (toString) wan.exposeIpv4Hosts
   );
 
-
   exposedIpv6Hosts = lib.strings.concatStringsSep "\n" (
     builtins.map (daddr: ''
       iifname { "${wanIface}" } ip6 daddr ${daddr} accept comment "Expose ${daddr} to WAN"
@@ -139,21 +81,20 @@ in
         chain input {
           type filter hook input priority 0; policy drop;
 
+          ct state { established, related } accept comment "Allow all established traffic"
           iifname { "${lanIface}" } accept comment "Allow local network to access the router"
-          iifname { "${wgIface}" } accept comment "Allow wireguard network to access the router"
+          iifname { "${wgIface}" } accept comment "Allow wireguard to access the router"
 
-          ${deniedVlanDhcpv4Access}
           ${routerAccess}
+          ${deniedVlanDhcpv4Access}
 
-          iifname "${wanIface}" ct state { established, related } accept comment "Allow established traffic"
           iifname "${wanIface}" icmp type { echo-request, destination-unreachable, time-exceeded } counter accept comment "Allow select ICMP"
 
           ${exposedUdpPorts}
           ${exposedTcpPorts}
 
-          iifname "${wanIface}" counter drop comment "Drop all other unsolicited traffic from wan"
-
           ${routerDenyAccess}
+          iifname "${wanIface}" counter drop comment "Drop all other unsolicited traffic from wan"
 
           iif lo accept comment "Allow all loopback traffic"
         }
@@ -161,17 +102,17 @@ in
         chain forward {
           type filter hook forward priority 0; policy drop;
 
-          ${exposedIpv4Hosts}
+          ct state { established, related } accept comment "Allow all established traffic"
+          iifname { "${lanIface}", "vlan*" } oifname { "${wanIface}" } accept comment "Allow all traffic going out"
 
-          ${interVlanRoutingBlock}
-          ${untaggedInterVlanBlock}
-
-          iifname { "${lanIface}" } oifname { "${wanIface}" } accept comment "Allow trusted LAN to WAN"
-          iifname { "${wanIface}" } oifname { "${lanIface}" } ct state established, related accept comment "Allow established back to LANs"
           iifname { "${lanIface}" } oifname { "${wgIface}" } accept comment "Allow LAN to wireguard"
           iifname { "${wgIface}" } oifname { "${lanIface}" } accept comment "Allow wireguard back to LANs"
 
-          ${routerForward}
+          iifname "vlan*" oifname "vlan*" drop comment "Drop inter-VLAN traffic"
+          iifname "${lanIface}*" oifname "vlan*" drop comment "Drop LAN to VLAN traffic"
+          iifname "vlan*" oifname "${lanIface}*" drop comment "Drop VLAN to LAN traffic"
+
+          ${exposedIpv4Hosts}
         }
       }
 
@@ -186,45 +127,45 @@ in
         chain input {
           type filter hook input priority 0; policy drop;
 
+          ct state { established, related } accept comment "Allow all established traffic"
           iifname { "${lanIface}" } accept comment "Allow local network to access the router"
           iifname { "${wgIface}" } accept comment "Allow wireguard to access the router"
-
-          ${deniedVlanDhcpv6Access}
           ${routerAccess}
+          ${deniedVlanDhcpv6Access}
 
-          iifname "${wanIface}" ct state { established, related } accept comment "Allow established traffic"
-          iifname "${wanIface}" icmpv6 type { echo-request, destination-unreachable, time-exceeded } counter accept comment "Allow select ICMP"
+          iifname { "${wanIface}", "${lanIface}", "vlan*" } icmpv6 type { 
+            destination-unreachable, packet-too-big, time-exceeded, 
+            parameter-problem, echo-request, echo-reply,
+            nd-router-advert, nd-neighbor-solicit, nd-neighbor-advert 
+          } accept comment "Allow essential ICMPv6"
 
           ${exposedUdpPorts}
           ${exposedTcpPorts}
 
+          ${routerDenyAccess}
           iifname "${wanIface}" counter drop comment "Drop all other unsolicited traffic from WAN"
 
-          iifname "${lanIface}" ip6 nexthdr icmpv6 icmpv6 type { nd-router-solicit, nd-router-advert, nd-neighbor-solicit, nd-neighbor-advert } counter accept comment "Allow SLAAC and DHCPv6"
-          ${routerIpv6IcmpAccess}
-
-          ${routerDenyAccess}
-
           iif lo accept comment "Allow all loopback traffic"
+        }
+
+        chain output {
+          type filter hook output priority 0; policy accept;
         }
 
         chain forward {
           type filter hook forward priority 0; policy drop;
 
-          ${exposedIpv6Hosts}
+          ct state { established, related } accept comment "Allow all established traffic"
+          iifname { "${lanIface}", "vlan*" } oifname { "${wanIface}" } accept comment "Allow all traffic going out"
 
-          ${interVlanRoutingBlock}
-          ${untaggedInterVlanBlock}
-
-          iifname { "${lanIface}" } oifname { "${wanIface}" } accept comment "Allow trusted LAN to WAN"
-          iifname { "${wanIface}" } oifname { "${lanIface}" } ct state established, related accept comment "Allow established back to LANs"
           iifname { "${lanIface}" } oifname { "${wgIface}" } accept comment "Allow LAN to wireguard"
           iifname { "${wgIface}" } oifname { "${lanIface}" } accept comment "Allow wireguard back to LANs"
 
-          ${routerForward}
+          iifname "vlan*" oifname "vlan*" drop comment "Drop inter-VLAN traffic"
+          iifname "${lanIface}*" oifname "vlan*" drop comment "Drop LAN to VLAN traffic"
+          iifname "vlan*" oifname "${lanIface}*" drop comment "Drop VLAN to LAN traffic"
 
-          iifname "${lanIface}" ip6 nexthdr icmpv6 icmpv6 type { nd-router-solicit, nd-router-advert, nd-neighbor-solicit, nd-neighbor-advert } accept comment "Allow essential ND in FORWARD"
-          ${routerIpv6NdForward}
+          ${exposedIpv6Hosts}
         }
       }
     '';
