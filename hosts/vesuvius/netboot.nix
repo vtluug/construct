dedaba6c4fb1f0792b288a8e0e330bd11b11f763
master <enzoisotton@wuvt.vt.edu>
2026-09-12 00:38:54 -0400

    removed dns/dhcp from vesuvius

--- hosts/vesuvius/netboot.nix
+++ hosts/vesuvius/netboot.nix
@@ -6,10 +6,7 @@
 }:
 let
   dom_ip = "10.98.3.2";
-  vlan_router_ip = "10.98.3.1";
-  dns_server_ip = "10.98.0.1";
-  dhcp_iface = "enp1s0f1";
-  client_range = "10.98.3.3,10.98.3.100";
+  tftp_iface = "enp1s0f1";
 
   netboot-hostnames = import ../bastille/blade-names.nix;
 
@@ -55,7 +52,7 @@ let
   '';
 in
 {
-  networking.interfaces."${dhcp_iface}".ipv4.addresses = [
+  networking.interfaces."${tftp_iface}".ipv4.addresses = [
     {
       address = dom_ip;
       prefixLength = 24;
@@ -65,42 +62,10 @@ in
   services.dnsmasq = {
     enable = true;
     settings = {
-      domain = "bastille.vtluug.org";
-      domain-needed = true;
-      interface = "${dhcp_iface}";
-      bind-interfaces = true;
-      server = [
-        "198.82.247.98"
-        "198.82.247.66"
-        "198.82.247.34"
-        "2001:468:c80:6101:0:100:0:62"
-        "2001:468:c80:4101:0:100:0:42"
-        "2001:468:c80:2101:0:100:0:22"
-        "/whit.vtluug.org/10.98.0.1"
-      ];
+      port = 0; #Disable DNS
+      interface = "${tftp_iface}";
       enable-tftp = true;
       tftp-root = "${tftproot}";
-      dhcp-range = "${client_range},12h";
-      dhcp-option = [ "option:router,${vlan_router_ip}" ];
-      dhcp-userclass = [ "set:ipxe,iPXE" ];
-      dhcp-boot = [
-        "tag:!ipxe,ipxe.efi"
-        "http://${dom_ip}:8080/boot.ipxe"
-      ];
-      # Set hostnames via DHCP
-      dhcp-host = builtins.map (host: "${host.fst},${host.snd}") (
-        lib.lists.filter (host: !lib.strings.hasInfix "unassigned" host.fst) (
-          lib.lists.zipLists (builtins.attrNames netboot-hostnames) (builtins.attrValues netboot-hostnames)
-        )
-      );
-      address = [
-        "/bastille.vtluug.org/::" # Filter IPv6 so it doesn't just hang forever when resolving every request to local domain
-        "/vesuvius.bastille.vtluug.org/${dom_ip}"
-        "/svc.bastille.vtluug.org/${dom_ip}"
-      ];
-      local = [
-        "/svc.bastille.vtluug.org/"
-      ];
     };
   };
 
