05409fcda041d1396d3bb4b96218223edaee6eba
master <enzoisotton@wuvt.vt.edu>
2026-09-12 15:47:06 -0400

    added all the blades mac addrs

--- hosts/bastille/blade-names.nix
+++ hosts/bastille/blade-names.nix
@@ -3,16 +3,15 @@
   "d8:9e:f3:3e:f9:41" = "prospit";
 
   "40:f2:e9:c6:65:5f" = "backbiter";
-  "40:f2:e9:c6:69:43" = "damocles";
-  "40:f2:e9:c6:69:67" = "durendal";
-  "40:f2:e9:c6:74:59" = "eyelander";
-  "40:f2:e9:c6:75:f1" = "excalibur";
-  "40:f2:e9:c6:76:21" = "gram";
-
-  "unassigned-0" = "gryffindor";
-  "unassigned-1" = "kusanagi";
-  "unassigned-2" = "narsil";
-  "unassigned-3" = "oathbringer";
-  "unassigned-4" = "riptide";
-  "unassigned-5" = "sting";
+  "40:f2:e9:c6:67:db" = "damocles";
+  "40:f2:e9:c6:68:95" = "durendal";
+  "40:f2:e9:c6:69:43" = "eyelander";
+  "40:f2:e9:c6:69:67" = "excalibur";
+  "40:f2:e9:c6:74:59" = "gram";
+  "40:f2:e9:c6:74:d7" = "gryffindor";
+  "40:f2:e9:c6:75:97" = "kusanagi";
+  "40:f2:e9:c6:75:9d" = "narsil";
+  "40:f2:e9:c6:75:f1" = "oathbringer";
+  "40:f2:e9:c6:76:21" = "riptide";
+  "40:f2:e9:c6:77:23" = "sting";
 }
